81093fb0a6df68e7b57c343f8766eab7fea65596
Jonatan Tran <jonatan.tran79@gmail.com>
2020-11-26 13:02:56 +0100

    Separated Methods in new classes

---
+++ TheHarbor/Register.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheHarbor
+{
+    class Register
+    {
+        Harbor harbor = new Harbor();
+        public void AddPowerBoatOnArrival(Boat[] harbor, int currentDay)
+        {
+            PowerBoat powerBoat = new PowerBoat();
+            powerBoat.DayOfArrival = currentDay;
+            powerBoat.DayToLeave = currentDay + powerBoat.DaysInHarbor;
+            RegisterBoat(harbor, powerBoat);
+        }
+        public void AddSailBoatOnArrival(Boat[] harbor, int currentDay)
+        {
+            SailBoat sailBoat = new SailBoat();
+            sailBoat.DayOfArrival = currentDay;
+            sailBoat.DayToLeave = currentDay + sailBoat.DaysInHarbor;
+            RegisterBoat(harbor, sailBoat);
+        }
+        public void AddCargoShipOnArrival(Boat[] harbor, int currentDay)
+        {
+            CargoShip cargoShip = new CargoShip();
+            cargoShip.DayOfArrival = currentDay;
+            cargoShip.DayToLeave = currentDay + cargoShip.DaysInHarbor;
+            RegisterBoat(harbor, cargoShip);
+        }
+        public void RegisterBoat(Boat[] harborList, Boat boat)
+        {
+            Reject reject = new Reject();
+            int emptySpace = harbor.SearchForEmptyHarborSpace(harborList);
+
+            if (emptySpace == -1)
+            {
+                reject.RejectBoatIfItDoesNotFitInHarbor(boat);
+            }
+            else if (harbor.CheckIfBoatFitsInHarbor(emptySpace, harborList, boat))
+            {
+                for (int i = 0; i < boat.HarborSpace; i++)
+                {
+                    harborList[emptySpace + i] = boat;
+                }
+            }
+        }
+    }
+}
---
+++ TheHarbor/Reject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHarbor
+{
+    class Reject
+    {
+        List<Boat> RejectedBoats = new List<Boat>();
+
+        public void RejectBoatIfItDoesNotFitInHarbor(Boat boat)
+        {
+            RejectedBoats.Add(boat);
+        }
+        public void ShowAllRejectedBoats()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"All rejected boats (count:{RejectedBoats.Count})");
+
+            foreach (var item in RejectedBoats)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
